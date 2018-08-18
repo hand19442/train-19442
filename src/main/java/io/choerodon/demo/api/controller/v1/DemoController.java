124925d61a383707fa73aaf2c27255ab8948bd84
彭冰冰 <bingbing.peng@hand-china.com>
2018-08-18 13:34:56 +0800

    提交代码

--- src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
+++ src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
@@ -1,6 +1,7 @@
 package io.choerodon.demo.api.controller.v1;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,14 @@ import io.choerodon.swagger.annotation.Permission;
 @RequestMapping("v1/demo")
 public class DemoController {
 
+    @Value("${user.username}")
+    private String username;
+    @Value("${user.number}")
+    private String number;
     @GetMapping("/hello")
     @Permission(level = ResourceLevel.SITE, permissionPublic = true)
     @ApiOperation(value = "demo")
     public ResponseEntity<String> hello() {
-        return new ResponseEntity<String>("hello world", HttpStatus.OK);
+        return new ResponseEntity<String>("hello,"+username+","+number, HttpStatus.OK);
     }
 }
